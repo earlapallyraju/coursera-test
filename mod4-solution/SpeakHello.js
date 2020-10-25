08c22e97afa26c8eee71ef65b4ea19c289382c47
earlapally raju <69709557+earlapallyraju@users.noreply.github.com>
2020-10-25 13:07:37 +0530

    Update SpeakHello.js

--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -16,12 +16,9 @@ var speakWord = "Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part
 helloSpeaker .speak=function(name){
-
-
-function speak(name) {
   console.log(speakWord + " " + name);
-}
-}
+};
+
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
